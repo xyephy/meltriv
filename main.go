fec3371b5f1761b7ec75b4023a17bb2b4a581984
xyephy <ziphyr.xyeag@gmail.com>
2020-05-22 19:53:59 +0300

    first commit

---
+++ main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"log"
+	"os"
+
+	tb "gopkg.in/tucnak/telebot.v2"
+)
+
+func main() {
+	var (
+		port      = os.Getenv("PORT")
+		publicURL = os.Getenv("PUBLIC_URL") // you must add it to your config vars
+		token     = os.Getenv("TOKEN")      // you must add it to your config vars
+	)
+
+	webhook := &tb.Webhook{
+		Listen:   ":" + port,
+		Endpoint: &tb.WebhookEndpoint{PublicURL: publicURL},
+	}
+
+	pref := tb.Settings{
+		Token:  token,
+		Poller: webhook,
+	}
+
+	b, err := tb.NewBot(pref)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
